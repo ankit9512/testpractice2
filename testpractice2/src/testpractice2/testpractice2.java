9508fa7819612db92774fdbb231fad2d73ec9e7c
AS57543 <AS57543@TRNW57SLPC0134.ind.zensar.com>
2019-09-23 12:53:25 +0530

    its java project

---
+++ testpractice2/src/testpractice2/testpractice2.java
@@ -0,0 +1,15 @@
+package testpractice2;
+
+public class testpractice2 {
+
+	public static void main(String[] args) {
+		   int [] arr = new int [] {1, 2, 3, 4, 5};  
+		   
+	        System.out.println("Elements of given array present on even position: ");    
+	        for (int i = 1; i < arr.length; i = i+2) {  
+	            System.out.println(arr[i]);  
+	        }  
+
+	}
+
+}
